c31dcd9c1ff431ffc06c237ba279182b3ade4cad
Andrew Gregg <andrewggregg@gmail.com>
2018-07-01 13:56:46 -0400

    implement A*

--- maze-solver.py
+++ maze-solver.py
@@ -1,21 +1,46 @@
 from square import *
+import heapq
 
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 with open("maze.txt") as maze:
     maze_array = [list(line.strip()) for line in maze]
 
-for x in range(len(maze_array[0])):
-    if (maze_array[0][x] == "_"):
-        start_square = Square(0,x)
-        break
-
 last_row = len(maze_array) - 1
+last_column = len(maze_array[0]) - 1
+
 for x in range(len(maze_array[last_row])):
     if (maze_array[last_row][x] == "_"):
-        end_square = Square(last_row,x)
+        end_square = Square(last_row,x,None,None)
         break
 
-print(maze_array)
-print(start_square.x)
-print(start_square.y)
-print(end_square.x)
-print(end_square.y)
+for x in range(len(maze_array[0])):
+    if (maze_array[0][x] == "_"):
+        start_square = Square(0,x,None,end_square)
+        break
+
+second_square = Square(start_square.x + 1, start_square.y, start_square, end_square)
+count = 0
+
+visited_squares = set([start_square])
+frontier = []
+heapq.heapify(frontier)
+heapq.heappush(frontier, (second_square.heuristic(), 0, second_square))
+while len(frontier):
+    heuristic, c, current_square = heapq.heappop(frontier)
+    visited_squares.add(current_square)
+    if current_square == end_square:
+        end_square = current_square
+        break
+    adjacent_squares = current_square.adjacent_squares(maze_array) - visited_squares
+    for adjacent_square in adjacent_squares:
+        count += 1
+        h = adjacent_square.heuristic()
+        heapq.heappush(frontier, (h, count, adjacent_square))
+
+for line in maze_array:
+    print(*line)
+
+curr = end_square
+while curr:
+    print('result %d,%d,distance%d' % (curr.x, curr.y, curr.distance_traversed))
+    curr = curr.parent
--- square.py
+++ square.py
@@ -1,7 +1,39 @@
 class Square:
-    def __init__(self, x, y):
+    def __init__(self, x, y, parent, end_square):
         self.x = x
         self.y = y
+        self.parent = parent
+        if end_square:
+            self.end_square = end_square
+            self.distance_to_end = self.distance(end_square)
+        if parent:
+            self.distance_traversed = parent.distance_traversed + 1
+        else:
+            self.distance_traversed = 0
 
-    def distance(square):
-        abs(square.x - x) + abs(square.y - y)
+    def __key(self):
+        return (self.x, self.y)
+
+    def __eq__(x, y):
+        return x.__key() == y.__key()
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def distance(self, square):
+        return abs(square.x - self.x) + abs(square.y - self.y)
+
+    def heuristic(self):
+        return self.distance_traversed + self.distance_to_end
+
+    def adjacent_squares(self, maze):
+        adjacent_squares = set()
+        if maze[self.x + 1][self.y] == "_":
+            adjacent_squares.add(Square(self.x + 1, self.y, self, self.end_square))
+        if maze[self.x - 1][self.y] == "_":
+            adjacent_squares.add(Square(self.x - 1, self.y, self, self.end_square))
+        if maze[self.x][self.y + 1] == "_":
+            adjacent_squares.add(Square(self.x, self.y + 1, self, self.end_square))
+        if maze[self.x][self.y - 1] == "_":
+            adjacent_squares.add(Square(self.x, self.y - 1, self, self.end_square))
+        return adjacent_squares
