1fc922bafc813c29118212bb7750a80181a9fc1c
Andrew Gregg <andrewggregg@gmail.com>
2018-07-01 08:55:10 -0400

    find start and end squares

--- maze-solver.py
+++ maze-solver.py
@@ -1,4 +1,21 @@
+from square import *
+
 with open("maze.txt") as maze:
     maze_array = [list(line.strip()) for line in maze]
 
+for x in range(len(maze_array[0])):
+    if (maze_array[0][x] == "_"):
+        start_square = Square(0,x)
+        break
+
+last_row = len(maze_array) - 1
+for x in range(len(maze_array[last_row])):
+    if (maze_array[last_row][x] == "_"):
+        end_square = Square(last_row,x)
+        break
+
 print(maze_array)
+print(start_square.x)
+print(start_square.y)
+print(end_square.x)
+print(end_square.y)
---
+++ square.py
@@ -0,0 +1,7 @@
+class Square:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(square):
+        abs(square.x - x) + abs(square.y - y)
